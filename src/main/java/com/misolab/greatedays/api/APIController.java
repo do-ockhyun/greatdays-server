d74239f99bd1affc6b6f32ba8f20dc5c0d0fdf85
do-ockhyun <do-ockhyun@naver.com>
2023-12-28 22:45:59 +0900

    project init

---
+++ src/main/java/com/misolab/greatedays/api/APIController.java
@@ -0,0 +1,16 @@
+package com.misolab.greatedays.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class APIController {
+
+    @GetMapping()
+    public String index() {
+        return "Hello, world!";
+    }
+    
+}
---
+++ src/test/java/com/misolab/greatedays/GreatedaysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.misolab.greatedays;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreatedaysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
